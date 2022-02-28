6149c23b638eb2f366ce78bab1088ca9479817d4
Phillip_Gen <phillip.kinney@generation.org>
2022-02-28 14:46:59 -0500

    fixing typo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 // Your application should meet the requirements listed below:
 // 1. Allows users to enter names into a text box.
 // 2. Allows users the enter the number of random names they want into a text box.
-// 3. A `Generate Random Names` button that intiates the randomization of the name in the text box.
+// 3. A `Generate Random Names` button that intiates the randomization of the names in the text box.
 // 4. The selected random names are displayed to the browser.
 // 5. Each time the `Generate Random Names` button is pressed, the previous names should be cleared, and a new selection of names should appear (instead of the new selection adding the prior list).
 // 5. The application should make sure that number entered by the user does not exceed the amount of names entered for randomization.
