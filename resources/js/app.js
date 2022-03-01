c7f4bf8a415d41c0b7a02e70294590395dc40879
Derrick James <95151579+flx1derrick@users.noreply.github.com>
2022-02-28 23:01:13 -0600

    initial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 // Your application should meet the requirements listed below:
-// 1. Allows users to enter names into a text box.
+// 1. Allows users to enter names into a text bodocument.getElementById("myText").value = "Johnny Bravo";
 // 2. Allows users the enter the number of random names they want into a text box.
 // 3. A `Generate Random Names` button that intiates the randomization of the name in the text box.
 // 4. The selected random names are displayed to the browser.
@@ -10,3 +10,34 @@
 // 8. The application should have a simple, visually pleasing design
 // Note: Do not spend too much time on design. Focus primarily on functionality(i.e. steps 1-8).
 // Be sure to check the helpful links in the README.
+const classRoster = ['Derrick','Valentina','Sri'];
+const generate = document.getElementById('generate-button');
+const clear = document.getElementById('clear-button');
+let names = document.getElementById('names');
+let inputElement = document.getElementById('how-many');
+let namesListElement = document.getElementById('names-list');
+
+//Take the value of the text area and put into an array; Take the value of #how-many names to generate; 
+
+function generateRandomName (){
+    let myArray = names.value.split(" ");
+    let numOfNames = inputElement.value;
+    if (numOfNames > myArray.length){
+        alert('Please enter a number less than ' + myArray.length)
+    }else{ for (let i = 0; i < numOfNames; i++) {
+            let randomName = myArray[Math.floor(Math.random() * myArray.length)];
+            const li = document.createElement('li');
+            li.innerHTML = randomName;
+            li.classList.add('alert', 'alert-primary')
+            namesListElement.appendChild(li);
+        }
+          
+    }
+};
+
+function clearNames () {
+    namesListElement.innerHTML = "";
+    };
+
+generate.addEventListener('click', generateRandomName);
+clear.addEventListener('click', clearNames);
